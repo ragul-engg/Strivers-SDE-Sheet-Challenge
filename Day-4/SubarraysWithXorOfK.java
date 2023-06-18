a29967099205cdb74e61786a8a2f8ceaf4c5bc78
ragul-engg <ragulshanmugarajan21@gmail.com>
2023-06-18 12:52:34 +0530

    completed day-4 challenges

---
+++ Day-4/SubarraysWithXorOfK.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraysWithXorOfK {
+    public static int subarraysXor(ArrayList<Integer> arr, int x) {
+        int cnt = 0;
+        Map<Integer, Integer> hash = new HashMap<>();
+        int len = arr.size();
+        int currVal = 0;
+        hash.put(0, 1);
+        for (int i = 0; i < len; i++) {
+            currVal = currVal ^ arr.get(i);
+            int required = currVal ^ x;
+            if (hash.containsKey(required)) {
+                cnt += hash.get(required);
+            }
+            hash.put(currVal, hash.getOrDefault(currVal, 0) + 1);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.addAll(Arrays.asList(4, 2, 2, 6, 4));
+        System.out.println(subarraysXor(al, 6));
+    }
+}
